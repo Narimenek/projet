9129cfad646664f89aec3628588d4bacc7bd2c1f
Yanis BOUSSADIA <147726290+yanis0641@users.noreply.github.com>
2024-01-06 23:17:10 +0100

    Add files via upload
    java fx (déconnexion,icone...)

---
+++ project/Projet/src/controller/HomeController.java
@@ -0,0 +1,151 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.scene.input.MouseEvent;
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class HomeController implements Initializable {
+
+	private Parent fxml;
+
+		@FXML
+		private AnchorPane root;
+		
+		@FXML
+	    private Label labelNomUtilisateur;
+
+	    private String loggedInUserName;
+
+	    public void setLoggedInUserName(String userName) {
+	        this.loggedInUserName = userName;
+	        labelNomUtilisateur.setText("Bienvenue,\n" + userName);
+
+	    }
+
+	    @FXML
+	    void accueil(MouseEvent event) {
+	    	try {
+				fxml = FXMLLoader.load(getClass().getResource("/interfaces/Accueil.fxml"));
+				root.getChildren().removeAll();
+				root.getChildren().setAll(fxml);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+
+	    }
+
+	    @FXML
+	    void contrat(MouseEvent event) {
+	    	try {
+				fxml = FXMLLoader.load(getClass().getResource("/interfaces/Contrat.fxml"));
+				root.getChildren().removeAll();
+				root.getChildren().setAll(fxml);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	    }
+
+	    @FXML
+	    void facture(MouseEvent event) {
+	    	try {
+				fxml = FXMLLoader.load(getClass().getResource("/interfaces/Factures.fxml"));
+				root.getChildren().removeAll();
+				root.getChildren().setAll(fxml);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	    }
+
+	    @FXML
+	    void historiques(MouseEvent event) {
+	    	try {
+				fxml = FXMLLoader.load(getClass().getResource("/interfaces/Historique.fxml"));
+				root.getChildren().removeAll();
+				root.getChildren().setAll(fxml);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	    }
+
+	    @FXML
+	    void locataire(MouseEvent event) {
+	    	try {
+				fxml = FXMLLoader.load(getClass().getResource("/interfaces/Locataire.fxml"));
+				root.getChildren().removeAll();
+				root.getChildren().setAll(fxml);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	    }
+
+	    @FXML
+	    void location(MouseEvent event) {
+	    	try {
+				fxml = FXMLLoader.load(getClass().getResource("/interfaces/Location.fxml"));
+				root.getChildren().removeAll();
+				root.getChildren().setAll(fxml);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	    }
+
+	    @FXML
+	    void voiture(MouseEvent event) {
+	    	try {
+				fxml = FXMLLoader.load(getClass().getResource("/interfaces/Voiture.fxml"));
+				root.getChildren().removeAll();
+				root.getChildren().setAll(fxml);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	    }
+	    @FXML
+	    void logout(MouseEvent event) {
+	        try {
+	            // Charger le fichier FXML associé à Main.fxml
+	            fxml = FXMLLoader.load(getClass().getResource("/interfaces/Main.fxml"));
+
+	            // Récupérer la scène à partir du nœud racine (root)
+	            AnchorPane currentRoot = (AnchorPane) root.getScene().getRoot();
+
+	            // Remplacer le nœud racine actuel par celui du Main.fxml
+	            currentRoot.getChildren().setAll(fxml);
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+
+
+
+
+
+
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+
+	}
+
+}
